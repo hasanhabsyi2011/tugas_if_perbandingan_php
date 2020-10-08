43a43c6d1f63c66c978763f81f1848fab01aab6a
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-08 12:34:57 +0700

    Add files via upload

---
+++ tugas_if_perbandingan.php
@@ -0,0 +1,17 @@
+<?php
+
+$a=10  ;
+$b=6.5;
+
+if ($a > $b):
+  echo $a." lebih besar dari ".$b;
+elseif ($a < $b):
+  echo $a." lebih kecil dari ".$b;
+elseif ($a = $b):
+  echo $a." sama dengan ".$b;
+endif;
+
+echo "<br>";
+
+
+ ?>
